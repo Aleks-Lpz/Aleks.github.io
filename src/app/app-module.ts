c34f5c7d31758e018fcfa5fcbe1d0f25299897aa
Aleks <aleks.lpx@gmail.com>
2026-03-04 02:48:52 -0600

    creacion de componentes y html Pt 1

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './header/header';
+import { WorkExperience } from './work-experience/work-experience';
+import { Education } from './education/education';
+import { Languages } from './languages/languages';
+import { Skills } from './skills/skills';
+import { Interest } from './interest/interest';
+import { Certificates } from './certificates/certificates';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    WorkExperience,
+    Education,
+    Languages,
+    Skills,
+    Interest,
+    Certificates
   ],
   imports: [
     BrowserModule,
